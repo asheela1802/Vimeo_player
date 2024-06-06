e943770cf51727edd2a30c5c34b2d500888ea556
Admin <karthikeyan@adrplexus.com>
2024-06-06 15:35:16 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/vimeo_player/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vimeo_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:vimeo_video_player/vimeo_video_player.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  // /// also support url like this: 'www.vimeo.com/70591644', 'vimeo.com/70591644'
+  // // final String _vimeoVideoUrl = 'https://vimeo.com/70591644';
+  // final String _vimeoVideoUrl = 'https://player.vimeo.com/video/841319969';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: VimeoVideoPlayer(
+          url: 'https://player.vimeo.com/video/953844236',
+          autoPlay: false,
+        ),
+      ),
+    );
+  }
+}
